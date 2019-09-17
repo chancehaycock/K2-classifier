c3b4013b7dd5aac5d450966d0870d6dbe4db09a3
Chance Haycock <chancehaycock@me.com>
2019-09-17 12:11:52 +0100

    Added Melbourne Data Example with Mean Absolute Error Calculation

--- introduction/intro.py
+++ introduction/intro.py
@@ -1,6 +1,21 @@
 # Introduction to Machine Learning with sklearn
 
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
-home_data = DecisionTreeRegressor()
+melbourne_data = pd.read_csv('introduction/melb_data.csv')
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+
+y = filtered_melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                      'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_features]
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(train_X, train_y)
+
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
