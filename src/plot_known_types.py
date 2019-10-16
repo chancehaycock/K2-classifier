89638e8db35d1c97bec542a10a15d9ab37ee4f22
Chance Haycock <chancehaycock@me.com>
2019-10-16 18:04:14 +0100

    Formatted Plots

--- src/plot_known_types.py
+++ src/plot_known_types.py
@@ -1,7 +1,7 @@
 from kepler_data_utilities import *
 
 star_types = ["RRab", "DSCUT", "EA", "EB", "GDOR", "Noise"]
-campaign_num = 4
+campaign_num = 3
 
 for type in star_types:
 	plot_ids = []
@@ -11,8 +11,10 @@ for type in star_types:
 	for i, ax in enumerate(axs.flat):
 		hdul = get_hdul(False, plot_ids[i])
 		times, flux = get_lightcurve(hdul, "PDC")
-		ax.plot(times[:200], flux[:200], linewidth=0.5)
-	fig.suptitle("Known {} stars from Campaign {} (Raw K2SC PDC Data ~ First 100 hours)".format(type, campaign_num))
+		ax.plot(times[:200], flux[:200]/1000, linewidth=0.5)
+	fig.suptitle("Known {} stars from c-0{} (Raw K2SC PDC Data ~ First 100 hours)".format(type, campaign_num))
 	plt.subplots_adjust(wspace=0.5, hspace=0.5)
+	fig.text(0.5, 0.02, 'Time (Days)', ha='center')
+	fig.text(0.02, 0.5, 'Flux / (10^3)', va='center', rotation='vertical')
 	plt.savefig("known_{}_c0{}_plot.png".format(type, campaign_num))
 	plt.close()
