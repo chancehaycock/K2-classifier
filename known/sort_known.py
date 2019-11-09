ef371df9d5aa6dbccab646d6a7fde05fc2994146
Chance Haycock <chancehaycock@me.com>
2019-11-09 11:00:41 +0000

    Change known IDs to shorter version

--- known/sort_known.py
+++ known/sort_known.py
@@ -14,14 +14,14 @@ import csv
 
 def write_to_file(kepler_id, campaign_num, star_type):
 	fout = open("{}_{}.txt".format(campaign_num, star_type), "a+")
-	fout.write("K2SC{}-c0{}\n".format(kepler_id, campaign_num))
+	fout.write("{}\n".format(kepler_id))
 	fout.close()
 
 with open("armstrong_0_to_4.csv") as knownfile:
 	reader = csv.DictReader(knownfile)
 	for row in reader:
 		star_type = row["Class"].split()[0]
-		kepler_id = row["# ID"]
+		kepler_id = row["epic_number"]
 		campaign_num = row["Campaign"]
 		write_to_file(kepler_id, campaign_num, star_type)
 
