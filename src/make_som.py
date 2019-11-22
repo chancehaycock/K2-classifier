fddd0fd6c2c52b86ba930de48a589e37150e9a65
Chance Haycock <chancehaycock@me.com>
2019-11-22 13:32:05 +0000

    Updated SOM Work
    The SOM bins are now normalised between 0 and 1 so that the min occurs at (0, 0)
    and the max value is 1.0. Some statistics are still needed to be generated on
    these binned values. Future work will involve printing the kohonen layer.
    
    Noise and OTHPER have been down-sampled to roughly 100 per som so that they
    do not dominate.
    
     All classes are now plotted.

--- src/make_som.py
+++ src/make_som.py
@@ -84,14 +84,26 @@ def make_2D_SOM_from_lightcurve(campaign_num):
 	known = campaign_is_known(campaign_num)
 
 	# Get SOM samples
-	df = pd.read_csv('{}/som_bins/campaign_{}.csv'\
+	df = pd.read_csv('{}/som_bins/campaign_{}_0_1_norm.csv'\
 	                 .format(px402_dir, campaign_num), 'r', delimiter=',')
 	# Remove any samples without complete entries.
 	df = df.dropna()
+
+	# Down Sampling SOM
+	noise_sample_df = df[df["Class"] == "Noise"].sample(n=100, random_state=1)
+	othper_sample_df = df[df["Class"] == "OTHPER"].sample(n=100, random_state=1)
+
+	df = df.drop(df[df["Class"] == "Noise"].index)
+	df = df.drop(df[df["Class"] == "OTHPER"].index)
+
+	df = pd.concat([df, othper_sample_df, noise_sample_df])
+	df = df.sort_index()
+
 	# Make copy of data frame without string entries (class) so that we can 
 	# convert it to a numpy array
 	if known:
 		df_without_class = df.drop("Class", axis=1)
+
 	# Variables needed for SOM object
 	# Size of Kohonen Layer. 40x40 for visual. 1600x1 for RF
 	som_shape = [40, 40]
@@ -109,6 +121,7 @@ def make_2D_SOM_from_lightcurve(campaign_num):
 	# Initialise and Plot
 	som = SimpleSOMMapper(som_shape, n_iter, initialization_func=init)
 	som.train(som_samples)
+	# TODO - PRINT THE KOHONEN LAYER!!!!!!
 	map = som(som_samples)
 	float_map = [[float(map[i][j]) + np.random.normal(0, 0.3) for j in range(2)]\
 	                                                   for i in range(len(map))] 
@@ -119,8 +132,6 @@ def make_2D_SOM_from_lightcurve(campaign_num):
 	GDOR = []
 	OTHPER = []
 	Noise = []
-	# TODO Maybe use colour map instead.
-	# Function?
 	for i in range(len(float_map)):
 		test_object = df.iloc[i]['Class'].split()[0]
 		if (test_object == 'EA'):
@@ -140,23 +151,33 @@ def make_2D_SOM_from_lightcurve(campaign_num):
 		else:
 			print("Error - no such class.")
 	if (len(EA) > 0):
-		plt.scatter(*zip(*EA), s=1.5, c='r', label='EA')
+		plt.scatter(*zip(*EA), s=2.5, c='r', label='EA')
 	if (len(EB) > 0):
-		plt.scatter(*zip(*EB), s=1.5, c='b', label='EB')
+		plt.scatter(*zip(*EB), s=2.5, c='b', label='EB')
 	if (len(RRab) > 0):
-		plt.scatter(*zip(*RRab), s=1.5, c='g', label='RRab')
+		plt.scatter(*zip(*RRab), s=2.5, c='g', label='RRab')
 	if (len(DSCUT) > 0):
-		plt.scatter(*zip(*DSCUT), s=1.5, c='y', label='DSCUT')
+		plt.scatter(*zip(*DSCUT), s=2.5, c='y', label='DSCUT')
 	if (len(GDOR) > 0):
-		plt.scatter(*zip(*GDOR), s=1.5, c='c', label='GDOR')
+		plt.scatter(*zip(*GDOR), s=2.5, c='c', label='GDOR')
 	if (len(OTHPER) > 0):
-		plt.scatter(*zip(*OTHPER), s=0.1, c='m', alpha=0.3)
+		plt.scatter(*zip(*OTHPER), s=0.1, c='m', label='OTHPER')
+	if (len(Noise) > 0):
+		plt.scatter(*zip(*Noise), s=0.1, c='k', label="Noise")
 	print("Total classified stars: ", len(EA) + len(EB) + len(RRab)\
 	                                + len(DSCUT) + len(GDOR))
+	print("Total Noise or OTHPER stares: ", len(Noise) + len(OTHPER))
+
 	plt.xlabel("SOM X Pixel")
 	plt.ylabel("SOM Y Pixel")
 	plt.legend()
 	plt.show()
+
+	# TODO -
+	# Generate SOM statistics here with the distances, and bmu etc.
+	# Also do the binned statistics here.
+
+
 
 def main():
 #	make_2D_SOM_from_features(3)
