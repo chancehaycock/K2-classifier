f53084c0fed693354b703922c1d2f484e6828622
Chance Haycock <chancehaycock@me.com>
2019-11-26 11:04:35 +0000

    Number of Changes - See below
    Fixed probability bug.
    - Duplications of probabilities were occuring in the final outputted csv
      due to missing a loop iteration. This never affected later resultsin SOM
      but was still incorrect in the outputted file.
    Everything is now done from the periods file.
    - The periods file is the starting point for all lightcurve processing.
      User has the option to plot - the original lc (polynomial fit)
                                  - the long-time detrended lc
                                  - the phasefold
                                  - the binned phasefold

--- src/process_lightcurve.py
+++ src/process_lightcurve.py
@@ -13,14 +13,22 @@ import matplotlib.gridspec as gridspec
 	# 2) K2SC lightcurve - fitted polynomial to remove long term variations
 	# 3) Phase folded version of the lightcurve
 	# 4) The binned lightcurve
+
 # I have found that these plots, along with the known campaign 3 classifications
 # are good for checking that the prigrams are running in the intended manner.
+
+# Needs to be done from period file and crossmatched with the classifications
+# from armstron et. al here.
 def process_lcs_for_som(campaign_num, process_outliers=True, write_to_csv=False,
                         plot=False):
+
+	# Is campaign known?
+	known_campaign = campaign_is_known(campaign_num) 
+
 	# Adds columns to the exported csv. Only true on first iteration.
 	add_bin_columns = True
 
-	# Known star types (with useful calssifications)
+	# Known star types (with useful classifications)
 	star_types = ["RRab", "DSCUT", "EA", "EB", "GDOR"]
 
 	# Set number of bins for the phasefolded lightcurve
@@ -29,17 +37,40 @@ def process_lcs_for_som(campaign_num, process_outliers=True, write_to_csv=False,
 	# Columns for the table
 	columns = make_bin_columns(n_bins)
 
-	data_file = '{}/tables/campaign_{}_master_table_flagged_on.csv'\
-	            .format(px402_dir, campaign_num)
-	df = pd.read_csv(data_file)
+#	data_file = '{}/tables/campaign_{}_master_table_flagged_on.csv'\
+#	            .format(px402_dir, campaign_num)
+#	df = pd.read_csv(data_file)
 
-	with open('{}/som_bins/campaign_{}_0_1_norm.csv'\
+	# Do everything from period file NOT master table. (Not created yet)
+	periods_file = '{}/periods/campaign_{}.csv'.format(project_dir,
+	                                                   campaign_num)
+	df = pd.read_csv(periods_file)
+	df = df[['epic_number', 'Period_1']]
+
+	# Load this if needed later.
+	if known_campaign:
+		classes_file = '{}/known/armstrong_0_to_4.csv'.format(project_dir)
+		classes_df = pd.read_csv(classes_file)
+
+	with open('{}/som_bins/campaign_{}.csv'\
 	           .format(px402_dir, campaign_num), 'a+') as file:
 		# Loop over all lightcurves in the campaign
 		for i in range(len(df['epic_number'])):
 			epic_num = int(df.iloc[i]['epic_number'])
 			period = df.iloc[i]['Period_1']
-			star_class = str(df.iloc[i]['Class'])
+
+			# If known fetch class and it probability
+			if known_campaign:
+				# Fetch star class here
+				class_row = classes_df[classes_df['epic_number'] == epic_num]
+				if (not class_row.empty):
+					star_class = class_row.iloc[0]["Class"]
+					probability = class_row.iloc[0][star_class]
+					star_class = star_class.split()[0]
+				else:
+					# Some mismatch in data sets.
+					star_class = ""
+					probability = -1
 
 			# Restriction on the max period here. Any period greater than 20
 			# does not have enough periodic information over the observation
@@ -92,56 +123,53 @@ def process_lcs_for_som(campaign_num, process_outliers=True, write_to_csv=False,
 				bin_means_max = np.nanmax(bin_means)
 				bin_means /= bin_means_max
 
-				if (plot):
-					if star_class.split()[0] in star_types:
-						print("Plotting")
-#						fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
-#						# Standard K2SC lc Plot
-#						ax1.plot(times, flux, linewidth=0.3)
-#						# The fitted polynomial superposed
-#						ax1.plot(times, polynomial, linewidth=1.5, c='m')
-#						# K2SC lc - polyfit
-#						ax2.plot(times, poly_flux, linewidth=0.3)
-#						# Phase folded lightcurve
-#						ax3.scatter(phase, normed_flux, s=0.2)
-#						# Binned Lightcure here
-#						ax4.scatter(bin_centres, bin_means, s=5)
-#
-						fig = plt.figure()
-						gs = fig.add_gridspec(4, 4)
-						ax1 = fig.add_subplot(gs[0, :])
-						ax1.plot(times, flux, linewidth=0.3)
-						ax1.plot(times, polynomial, linewidth=1.5, c='m')
-						ax2 = fig.add_subplot(gs[1, :])
-						ax2.plot(times, poly_flux, linewidth=0.3)
-						ax3 = fig.add_subplot(gs[2:, :2])
-						ax3.scatter(phase, normed_flux, s=0.2)
-						ax4 = fig.add_subplot(gs[2:, 2:])
-						ax4.scatter(bin_centres, bin_means, s=5)
-						ax4.set_ylim([0, 1])
+				if known_campaign:
+					if (plot):
+						if star_class in star_types:
+							print("Plotting")
 
-						fig.suptitle("Automated Period: {}".format(period))
-						plot_dir = "{}/plots/phase_folds_4_way/"\
-						           .format(px402_dir)
-						plt.tight_layout()
-						plt.subplots_adjust(top=0.85)
-						plt.savefig("{}/{}_processing_plot_{}_c{}.png"\
-						.format(plot_dir, star_class, epic_num, campaign_num))
-						plt.close()
+							fig = plt.figure()
+							gs = fig.add_gridspec(4, 4)
+							ax1 = fig.add_subplot(gs[0, :])
+							# Standard K2SC lc Plot
+							ax1.plot(times, flux, linewidth=0.3)
+							# The fitted polynomial superposed
+							ax1.plot(times, polynomial, linewidth=1.5, c='m')
+							ax2 = fig.add_subplot(gs[1, :])
+							# K2SC lc - polyfit
+							ax2.plot(times, poly_flux, linewidth=0.3)
+							ax3 = fig.add_subplot(gs[2:, :2])
+							# Phase folded lightcurve
+							ax3.scatter(phase, normed_flux, s=0.2)
+							ax4 = fig.add_subplot(gs[2:, 2:])
+							# Binned Lightcure here
+							ax4.scatter(bin_centres, bin_means, s=5)
+							ax4.set_ylim([0, 1])
+
+							fig.suptitle("Automated Period: {}".format(period))
+							plot_dir = "{}/plots/phase_folds_4_way/"\
+							           .format(px402_dir)
+							plt.tight_layout()
+							plt.subplots_adjust(top=0.85)
+							plt.savefig("{}/{}_processing_plot_{}_c{}.png"\
+							.format(plot_dir, star_class, epic_num, campaign_num))
+							plt.close()
 			else:
 				# Nonsense. Gets filtered out at SOM creation stage anyway.
 				bin_means = np.empty(n_bins) * np.nan 
 
-
 			# ======================
 			# Option to write to CSV
 			# ======================
-			star_class = star_class.split()[0]
 			if (write_to_csv):
+				# Adds rows of SOM Bins Applicable to all.
 				row = pd.DataFrame(bin_means.reshape(-1, len(bin_means)),
 				                   columns=columns)
 				row['epic_number'] = epic_num
-				row['Class'] = star_class.split()[0]
+				# Add additional info for known stars
+				if known_campaign:
+					row['Class'] = star_class
+					row['Probability'] = probability 
 				if (add_bin_columns):
 					add_bin_columns = False
 					row.to_csv(file, index=None)
@@ -150,7 +178,9 @@ def process_lcs_for_som(campaign_num, process_outliers=True, write_to_csv=False,
 
 			# Give the user a progress bar
 			if i%50 == 0:
-				print("{}%".format(float(100*i/16882)))
+				size = len(df['epic_number'])
+				print("{0:.2f}%".format(float(100*i/size)))
+
 	# Confirmation of file compeletion
 	print("{} created.".format(file))
 	return None
@@ -162,7 +192,10 @@ def process_lcs_for_som(campaign_num, process_outliers=True, write_to_csv=False,
 
 def main():
 	print("Running...")
-	process_lcs_for_som(3, process_outliers=True, write_to_csv=True, plot=True)
+#	process_lcs_for_som(3, process_outliers=True, write_to_csv=True, plot=True)
+#	process_lcs_for_som(3, process_outliers=True, write_to_csv=True, plot=False)
+#	process_lcs_for_som(4, process_outliers=True, write_to_csv=True, plot=True)
+#	process_lcs_for_som(4, process_outliers=True, write_to_csv=True, plot=False)
 
 if __name__ == "__main__":
 	main()
