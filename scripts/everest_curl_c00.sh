855fa6fbeffa0acd85a6e125e691a4a146e30677
Chance Haycock <chancehaycock@me.com>
2019-12-17 17:57:07 +0000

    Added Everest Download Scripts

---
+++ scripts/everest_curl_c00.sh
@@ -0,0 +1,2 @@
+curl -O https://archive.stsci.edu/hlsps/everest/v2/c00/202000000/73186/hlsp_everest_k2_llc_202073186-c00_kepler_v2.0_lc.fits
+curl -O https://archive.stsci.edu/hlsps/everest/v2/c00/202000000/73345/hlsp_everest_k2_llc_202073345-c00_kepler_v2.0_lc.fits
