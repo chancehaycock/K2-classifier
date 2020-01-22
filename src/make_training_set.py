acd02a9532bd3f65365e1efe30d125af420e7b01
Chance Haycock <chancehaycock@me.com>
2020-01-22 14:31:55 +0000

    Update Training Set for Campaaigns 1-4

--- src/make_training_set.py
+++ src/make_training_set.py
@@ -117,9 +117,64 @@ def make_RF_training_set(write_to_csv=False):
 	print(len(OTHPER_df))
 	print(len(Noise_df))
 
+
+def make_manual_k2sc_training_set(write_to_csv=False):
+
+	# Open Master Table
+	data_file = "{}/known/armstrong_0_to_4.csv".format(px402_dir)
+	df   = pd.read_csv(data_file)
+
+	# Restrict to campaigns only 1, 2, 3, 4
+	df = df[df["Campaign"] != 0]
+
+	RRab_df   = df[df["Class"] == "  RRab"].dropna().sort_values("  RRab", ascending=False)
+	EA_df     = df[df["Class"] == "    EA"].dropna().sort_values("    EA", ascending=False)
+	EB_df     = df[df["Class"] == "    EB"].dropna().sort_values("    EB", ascending=False)
+	DSCUT_df  = df[df["Class"] == " DSCUT"].dropna().sort_values(" DSCUT", ascending=False)
+	GDOR_df   = df[df["Class"] == "  GDOR"].dropna().sort_values("  GDOR", ascending=False)
+	OTHPER_df = df[df["Class"] == "OTHPER"].dropna().sort_values("OTHPER", ascending=False)
+	Noise_df  = df[df["Class"] == " Noise"].dropna().sort_values(" Noise", ascending=False)
+
+	# Initial set of thresholds
+	# This one below is used for k2_1-4_training_set_download.sh 
+	thresholds = np.array([0.6, 0.7, 0.9, 0.89, 0.66, 0.990, 0.981])
+
+	# Choose Probability Thresholds here
+	RRab_df   =   RRab_df[  RRab_df["  RRab"] > thresholds[0]]
+	EA_df     =     EA_df[    EA_df["    EA"] > thresholds[1]]
+	EB_df     =     EB_df[    EB_df["    EB"] > thresholds[2]]
+	DSCUT_df  =  DSCUT_df[ DSCUT_df[" DSCUT"] > thresholds[3]]
+	GDOR_df   =   GDOR_df[  GDOR_df["  GDOR"] > thresholds[4]]
+	OTHPER_df = OTHPER_df[OTHPER_df["OTHPER"] > thresholds[5]]
+	Noise_df  =  Noise_df[ Noise_df[" Noise"] > thresholds[6]]
+	
+	t_set = RRab_df.append(EA_df).append(EB_df).append(DSCUT_df).append(GDOR_df)\
+	                      .append(OTHPER_df).append(Noise_df).sort_values("epic_number")\
+	                      [['epic_number', 'Campaign']]
+	
+	print(t_set)
+
+	output_file = "{}/training_sets/1-4_training_set.csv".format(project_dir)
+
+	if (write_to_csv):
+		t_set.to_csv("{}".format(output_file), index=False)
+
+		# Now produce script to download these epic in k2 raw data.
+		script_file = "{}/scripts/antares_k2_1-4_download.sh".format(project_dir)
+		with open(script_file, 'a') as ofile:
+			for entry in t_set.to_numpy():
+				epic = str(entry[0])
+				campaign_num = entry[1]
+
+				command = "curl -O https://archive.stsci.edu/missions/k2/lightcurves/c{}/{}00000/{}000/ktwo{}-c0{}_llc.fits"\
+				            .format(campaign_num, epic[0:4], epic[4:6], epic, campaign_num)
+				ofile.write("{}\n".format(command))
+
+
 def main():
 #	make_SOM_training_set(write_to_csv=False)
-	make_RF_training_set(write_to_csv=True)
+#	make_RF_training_set(write_to_csv=True)
+#	make_manual_k2sc_training_set(write_to_csv=True)
 
 if __name__ == "__main__":
 	main()
