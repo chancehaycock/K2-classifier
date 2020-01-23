2074ee4a4a12f587e6c4fa86e3578b125a74a8ac
Chance Haycock <chancehaycock@me.com>
2020-01-23 13:58:54 +0000

    Script to plot Manual K2SC Training Set

--- src/plot_manual_k2sc_lightcurve.py
+++ src/plot_manual_k2sc_lightcurve.py
@@ -1,28 +1,30 @@
 from kepler_data_utilities import *
+import os 
 
-#hdul=fits.open('pipelines/k2sc/example_run/EPIC_205938750_mast.fits')
-hdul=fits.open('pipelines/k2sc/example_run/EPIC_201129544_mast.fits')
-lc_type_index='PDC'
-hdul2=fits.open('pipelines/k2sc/example_run/ktwo201129544-c01_llc.fits')
+file_dir = '/Users/chancehaycock/dev/machine_learning/px402/lightcurve_data/k2sc/campaign_1-2_manual'
 
-time = hdul2[1].data['TIME']
-sap_fluxes = hdul2[1].data['SAP_FLUX']
-pdcsap_fluxes = hdul2[1].data['PDCSAP_FLUX']
-plt.scatter(time, sap_fluxes, s=0.5, label='sap')
-plt.scatter(time, pdcsap_fluxes, s=0.5, label='pdc')
-plt.legend()
-plt.show()
-plt.close()
+for filee in os.listdir(file_dir):
+	epic = filee[5:14]
+	print(epic)
+	hdul=fits.open('{}/{}'.format(file_dir, filee))
 
-raw_flux = hdul[1].data['flux']
-trend_t = hdul[1].data['trtime']
-times = hdul[1].data['time']
-mflags = hdul[1].data['mflags']
-err = hdul[1].data['error']
+	raw_flux = hdul[1].data['flux']
+	trend_t = hdul[1].data['trtime']
+	times = hdul[1].data['time']
+	mflags = hdul[1].data['mflags']
+	err = hdul[1].data['error']
 
-flux = raw_flux + trend_t - np.median(trend_t)
+	flux = raw_flux + trend_t - np.median(trend_t)
 
-plt.scatter(times, raw_flux, s=0.5, label='sap_raw')
-plt.scatter(times, flux, s=0.5, label='sap_k2sc')
-plt.legend()
-plt.show()
+	use_times, use_flux = get_lightcurve(hdul)
+
+	fig, (ax1, ax2) = plt.subplots(2, 1)
+	ax1.scatter(times, raw_flux, s=0.5, label='pdc')
+	ax1.scatter(times, flux, s=0.5, label='k2sc', c='orange')
+	fig.suptitle("EPIC {}".format(epic))
+	ax1.legend()
+	ax2.scatter(use_times, use_flux, s=0.5, label='k2sc - processed outliers', c='orange')
+	ax2.legend()
+
+	plt.savefig('{}/plots/k2sc_vs_pdc_c1-2_attempt2/{}_k2sc_vs_pdc.png'.format(project_dir, epic))
+	plt.close()
